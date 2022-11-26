e72b59503c473c1da792bd9ae848b2f012203a8c
Scott Wegley <scbrwegley@protonmail.ch>
2022-11-25 20:55:25 -0800

    moved default packets into command for good practice

--- src/App.java
+++ src/App.java
@@ -16,7 +16,8 @@ public class App {
         String[] comPorts = VEXnetDriver.availableComPorts();
         if (comPorts.length == 0) {
             System.out.println("There are no com ports available.");
-            //return; //Uncomment for final version.  If testing w/o robot, leave commented as will likely prematurely end program.
+            // return; //Uncomment for final version. If testing w/o robot, leave commented
+            // as will likely prematurely end program.
         } else {
             System.out.println("Using: " + comPorts[0]);
             driver = new VEXnetDriver(comPorts[0], VEXnetDriver.DeviceType.VEXnet_Joystick_Partner_Port);
@@ -25,9 +26,9 @@ public class App {
     }
 }
 
-/* 
+/*
  * A Thread-Like class to store a FIFO queue of commands.
- * On run, these commands are iterated through and executed. 
+ * On run, these commands are iterated through and executed.
  * Previous commands must finish before continuing to the next.
  */
 class Scheduler extends Thread {
@@ -77,7 +78,7 @@ class ReturnLogger extends Thread {
         }
     }
 
-    public void deactivate(){
+    public void deactivate() {
         active = false;
     }
 }
@@ -86,23 +87,79 @@ class ReturnLogger extends Thread {
  * Thread that contains a VEXnetPacket to send.
  * The thread is paused for a specified duration after sending.
  * Then a packet to stop all motors is sent before ending.
- * This last packets makes the transition to the next command smoother on the motors.
+ * This last packets makes the transition to the next command smoother on the
+ * motors.
  */
-class Command extends Thread implements DefaultPackets {
+class Command extends Thread {
     private VEXnetPacket toExecute;
     private long duration;
     private static VEXnetDriver driver;
 
+    static VEXnetPacket FULL_STOP = VEXnetPacket.compileControllerPacket(
+            (byte) (127), (byte) (127), (byte) (127), (byte) (127),
+            false, false,
+            false, false,
+            false, false, false, false,
+            false, false, false, false,
+            (byte) 127, (byte) 127, (byte) 127);
+
+    static VEXnetPacket FULL_FORWARD = drive(255, 255);
+
+    static VEXnetPacket FULL_REVERSE = drive(0, 0);
+
+    static VEXnetPacket FULL_OPEN_CLAW = claw(255);
+
+    static VEXnetPacket FULL_CLOSE_CLAW = claw(0);
+
+    private static VEXnetPacket drive(int i1, int i2) {
+        if (i1 <= 255 && i1 >= 0 && i2 <= 255 && i2 >= 0) {
+            return VEXnetPacket.compileControllerPacket(
+                    (byte) (i1), (byte) (i2), (byte) (127), (byte) (127),
+                    false, false,
+                    false, false,
+                    false, false, false, false,
+                    false, false, false, false,
+                    (byte) 127, (byte) 127, (byte) 127);
+        } else
+            return FULL_STOP;
+    }
+
+    private static VEXnetPacket arm(int i1) {
+        if (i1 <= 255 && i1 >= 0) {
+            return VEXnetPacket.compileControllerPacket(
+                    (byte) (127), (byte) (127), (byte) (i1), (byte) (127),
+                    false, false,
+                    false, false,
+                    false, false, false, false,
+                    false, false, false, false,
+                    (byte) 127, (byte) 127, (byte) 127);
+        } else
+            return FULL_STOP;
+    }
+
+    private static VEXnetPacket claw(int i1) {
+        if (i1 <= 255 && i1 >= 0) {
+            return VEXnetPacket.compileControllerPacket(
+                    (byte) (127), (byte) (127), (byte) (127), (byte) (i1),
+                    false, false,
+                    false, false,
+                    false, false, false, false,
+                    false, false, false, false,
+                    (byte) 127, (byte) 127, (byte) 127);
+        } else
+            return FULL_STOP;
+    }
+
     /**
      * @param exec A VexnetPacket to be executed
-     * @param dur How long to execute in milliseconds.
+     * @param dur  How long to execute in milliseconds.
      */
     public Command(VEXnetPacket exec, long dur) {
         this.toExecute = exec;
         this.duration = dur;
     }
 
-    public static void setDriver(VEXnetDriver driv){
+    public static void setDriver(VEXnetDriver driv) {
         driver = driv;
     }
 
