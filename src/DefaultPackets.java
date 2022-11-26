53ace333d917e1b407bda52aaf5521dd8efc3d5d
Scott Wegley <scbrwegley@protonmail.ch>
2022-11-25 20:23:34 -0800

    packet for stopping (all defualt values)

--- src/DefaultPackets.java
+++ src/DefaultPackets.java
@@ -1,4 +1,11 @@
 import VexnextDriver.VEXnetPacket;
 
 public interface DefaultPackets {
+    VEXnetPacket FULL_STOP = VEXnetPacket.compileControllerPacket(
+            (byte) (127), (byte) (127), (byte) (127), (byte) (127),
+            false, false,
+            false, false,
+            false, false, false, false,
+            false, false, false, false,
+            (byte) 127, (byte) 127, (byte) 127);
 }
