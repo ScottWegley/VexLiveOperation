ddf77d0401815e39834e4c2450e414b1183f20f1
Scott Wegley <scbrwegley@protonmail.ch>
2022-11-25 20:23:49 -0800

    function to construct driving packets

--- src/DefaultPackets.java
+++ src/DefaultPackets.java
@@ -8,4 +8,15 @@ public interface DefaultPackets {
             false, false, false, false,
             false, false, false, false,
             (byte) 127, (byte) 127, (byte) 127);
+    private static VEXnetPacket drive(int i1, int i2) {
+        if (i1 <= 255 && i1 >= 0 && i2 <= 255 && i2 >= 0) {
+            return VEXnetPacket.compileControllerPacket(
+                    (byte) (i1), (byte) (i2), (byte) (127), (byte) (127),
+                    false, false,
+                    false, false,
+                    false, false, false, false,
+                    false, false, false, false,
+                    (byte) 127, (byte) 127, (byte) 127);
+        } else return FULL_STOP;
+    }
 }
