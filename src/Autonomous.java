daba49a103bc6c98a07705760bf0994bea25c6c9
scottwegley <scottwegley@gmail.com>
2022-11-30 22:39:58 -0800

    reorganized for sensible var initialization of driver

--- src/Autonomous.java
+++ src/Autonomous.java
@@ -2,8 +2,8 @@ import VexnetDriver.VEXnetDriver;
 
 public class Autonomous {
 
-    public static VEXnetDriver driver = null;
-    private static Scheduler scheduler = new Scheduler();
+    public static VEXnetDriver driver;
+    private static Scheduler scheduler;
 
     public static void main(String[] args) {
 
@@ -15,9 +15,9 @@ public class Autonomous {
         } else {
             System.out.println("Using: " + comPorts[0]);
             driver = new VEXnetDriver(comPorts[0], VEXnetDriver.DeviceType.VEXnet_Joystick_Partner_Port);
+            scheduler = new Scheduler();
+            Command.setDriver(driver);
         }
-
-        Command.setDriver(driver);
 
         scheduler.add(new Command(PacketBuilder.FULL_FORWARD, 2500));
         scheduler.add(new Command(PacketBuilder.FULL_STOP, 1000));
